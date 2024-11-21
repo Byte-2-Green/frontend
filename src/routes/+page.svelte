2beab2203ea6165778f46355685c56c0841d1ca1
popo0015 <popo0015@hz.nl>
2024-11-21 14:58:50 +0100

    Fixed the footer/navigation to look more like the prototype.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,20 @@
 <script>
-    import Header from '../components/Header.svelte';
-    import StatsPanel from '../components/StatsPanel.svelte';
-    import Gallery from '../components/Gallery.svelte';
-    import Footer from '../components/Footer.svelte';
-  
+    import Header from "../components/Header.svelte";
+    import StatsPanel from "../components/StatsPanel.svelte";
+    import Gallery from "../components/Gallery.svelte";
+    import Footer from "../components/Footer.svelte";
+
+    import "../app.css";
+
     let unlockedFrames = 4;
     let savedCO2 = 0.5; // kg
-  </script>
-  
-  <div class="flex flex-col h-screen bg-background-light">
+</script>
+
+<div class="flex flex-col h-screen bg-orange-50">
     <Header />
     <main class="flex-1 overflow-y-auto">
-      <StatsPanel {unlockedFrames} {savedCO2} />
-      <Gallery />
+        <StatsPanel {unlockedFrames} {savedCO2} />
+        <Gallery />
     </main>
     <Footer />
-  </div>
-  
+</div>
