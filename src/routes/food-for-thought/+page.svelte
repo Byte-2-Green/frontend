6ae83cad5adeca12bc757c788f1f15010bb5fb38
Alexandra <smir0002@hz.nl>
2024-11-25 15:18:16 +0100

    facts api fetch working

---
+++ src/routes/food-for-thought/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { onMount } from "svelte";
+// @ts-nocheck
+
+    /** * @type {{ description: string }[]} */
+    export let foodForThought = [];
+    console.log(foodForThought);
+
+    onMount(async() => {
+        const res = await fetch(`http://localhost:3011/foodForThought`);
+        const data = await res.json();
+        console.log(data);
+    })
+</script>
+
+
+<h1>Food for Thought</h1>
+{#if foodForThought.length > 0}
+    <ul>
+        {#each foodForThought as thought}
+            <li>
+                <p>{thought.description}</p>
+            </li>
+        {/each}
+    </ul>
+{:else}
+    <p>Loading...</p>
+{/if}
