d257ed112dc7bcfcfafd9da994fb9e9ba6cf8ae2
Alexandra <smir0002@hz.nl>
2024-11-27 11:24:38 +0100

    api and content passed to the main page of the platform, working good

--- src/routes/food-for-thought/+page.svelte
+++ src/routes/food-for-thought/+page.svelte
@@ -1,6 +1,7 @@
 <script>
+// @ts-nocheck
+
     import { onMount } from "svelte";
-    // @ts-nocheck
 
     // variable to fetch the array of thoughts from the api
     /** * @type {{ Description: string }[]} */
@@ -31,6 +32,9 @@
 <h1>Food for Thought</h1>
 <!-- loops through the whole array and picks a random thought to be displayed-->
 {#if randomThought}
+<div class="flex justify-center mb-4 ml-4">
+    <i class="{randomThought.Icon} w-20 h-20 text-6xl"></i>
+</div>
     <p>{randomThought.Description}</p>
 {:else}
     <p>Loading...</p>
