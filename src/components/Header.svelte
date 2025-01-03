6bb4febd3dbe0fafcd0549d8dcbfb770fe88ec70
popo0015 <popo0015@hz.nl>
2025-01-03 16:03:42 +0100

    Fixing the header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,7 +9,7 @@
       <p class="text-sm text-gray-400">@janed.museum</p>
     </div>
   </div>
-  <div class="relative cursor-pointer">
+  <div class="relative -mt-10 mr-3 cursor-pointer">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14V9a6 6 0 00-12 0v5c0 .386-.149.735-.405 1.005L4 17h5m6 0a3 3 0 11-6 0m6 0H9" />
     </svg>
